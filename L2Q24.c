3a7c077591a5636ebbd5f7ba61649fe23e97b59b
Arthur Dantas <141188240+ArthurDantasL@users.noreply.github.com>
2023-09-13 23:25:47 -0300

    Update L2Q24.c

--- L2Q24.c
+++ L2Q24.c
@@ -1,6 +1,23 @@
+//L2-Q24:
+
 #include <stdio.h>
+#include <math.h>
+#define MAX 500
 
 int main() {
+    int a = 1, b = 1, c = 1;
+    
+    for (c; c <= MAX; c++) {
+        a = 1; b = 1;
+        for (b; b <= MAX; b++) {
+            a = 1;
+            for (a; a <= MAX; a++) {
+                if (pow(a, 2) + pow(b, 2) == pow(c, 2)) {
+                    printf("%d² + %d² = %d²\n", a, b, c);
+                }
+            }
+        }        
+    }
     
     return 0;
 }
